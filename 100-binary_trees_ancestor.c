b73d1c67fff637b554ba3b3d6c2b7dc7819cd341
21Kgold <claudiarpa@gmail.com>
2022-07-21 13:08:02 -0700

    update

---
+++ 100-binary_trees_ancestor.c
@@ -0,0 +1,65 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_depth - measures the depth of a node in a binary tree
+ * @tree:  pointer to the root node of the tree to traverse
+ * Return: depth of a node in a binary tree
+*/
+
+size_t binary_tree_depth(const binary_tree_t *tree)
+{
+	int d = 0;
+
+	if (tree != NULL)
+	{
+		if (tree->parent)
+		{
+			d = binary_tree_depth(tree->parent) + 1;
+		}
+	}
+	return (d);
+}
+
+/**
+ * binary_trees_ancestor - finds the lowest common ancestor of two nodes
+ * @first: pointer to the node to check for lowest ancestor
+ * @second: pointer to the node to check for lowest ancestor
+ * Return:  pointer to the lowest common ancestor node of the two given nodes
+ * If no common ancestor was found, return NULL
+*/
+
+binary_tree_t *binary_trees_ancestor(const binary_tree_t *first,
+	const binary_tree_t *second)
+{
+	size_t depth1 = binary_tree_depth(first);
+	size_t depth2 = binary_tree_depth(second);
+
+	if (first == NULL || second == NULL)
+		return (NULL);
+
+	if (depth1 < depth2)
+		if (are_you_my_ancestor(first, second) == first)
+			return ((binary_tree_t *)first);
+		else
+			return (first->parent);
+	else
+		if (are_you_my_ancestor(second, first) == second)
+			return ((binary_tree_t *)second);
+		else
+			return (second->parent);
+}
+
+binary_tree_t *are_you_my_ancestor(const binary_tree_t *first,
+	const binary_tree_t *second)
+{
+	if (first && second)
+	{
+		if (second->parent == first)
+			return ((binary_tree_t *)first);
+		if (second->parent == NULL)
+			return (NULL);
+		if (second->parent)
+			return (are_you_my_ancestor(first, second->parent));
+	}
+	return (NULL);
+}
