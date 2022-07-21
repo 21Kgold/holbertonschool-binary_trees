b6a11c2e63a367f78f965228ef2143ffcddc34f8
21Kgold <claudiarpa@gmail.com>
2022-07-21 09:30:26 -0700

    update edge case

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -79,7 +79,7 @@ int binary_tree_is_full(const binary_tree_t *tree)
 
 int binary_tree_is_perfect(const binary_tree_t *tree)
 {
-	if (!tree || (!tree->left || !tree->right))
+	if (!tree || (!tree->left && !tree->right))
 		return (0);
 	if (binary_tree_balance(tree) == 0 && binary_tree_is_full(tree) == 1)
 		return (1);
