71ffaef94b3d6c0321d39d2eb655194e246479d4
21Kgold <claudiarpa@gmail.com>
2022-07-21 00:55:08 -0700

    update

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -12,19 +12,37 @@
 */
 
 int binary_tree_balance(const binary_tree_t *tree)
+
+{
+	int height_l = 0, height_r = 0;
+
+	if (!tree)
+		return (0);
+
+	height_l = binary_tree_height(tree->left);
+	height_r = binary_tree_height(tree->right);
+
+	return (height_l - height_r);
+}
+
+/**
+ * binary_tree_height - measures the height of a binary tree
+ * @tree:  pointer to the root node of the tree to traverse
+ * Return: height of a binary tree
+*/
+
+size_t binary_tree_height(const binary_tree_t *tree)
 {
 	int l = 0, r = 0;
 
 	if (!tree)
 		return (0);
 
-	if (tree->left)
-	{
-		l = binary_tree_balance(tree->left) + 1;
-	}
-	if (tree->right)
-	{
-		r = binary_tree_balance(tree->right) + 1;
-	}
-	return (l - r);
+	l = binary_tree_height(tree->left) + 1;
+	r = binary_tree_height(tree->right) + 1;
+
+	if (l >= r)
+		return (l);
+	else
+		return (r);
 }
